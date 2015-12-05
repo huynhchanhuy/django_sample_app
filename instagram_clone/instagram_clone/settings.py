f9ba57fb54df237fd4a716f473d10ab605b2dbcb
Huy <huynhchanhuy@gmail.com>
2015-12-05 19:59:58 +0700

    fix registration

--- instagram_clone/instagram_clone/settings.py
+++ instagram_clone/instagram_clone/settings.py
@@ -32,11 +32,13 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photofeeds',
+    'registration',
     'crispy_forms',
 )
 
@@ -101,6 +103,8 @@ EMAIL_HOST_USER = 'huyhuynh.test@gmail.com'
 EMAIL_HOST_PASSWORD = 'bnbhbtppfixtlyvw'
 EMAIL_USE_TLS = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/' #URL representation on Web
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_env","static_root") #Link to static files
 
@@ -110,14 +114,14 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_env","media_root")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static_dirs","static_files"),]
 
+# Crispy Form Tags Settings
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-REGISTRATION_OPEN = True                # If True, users can register
+#REGISTRATION_OPEN = True                # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
-LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
-LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
- 
+#LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+#LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+
+SITE_ID = 1 # Login
--- instagram_clone/instagram_clone/urls.py
+++ instagram_clone/instagram_clone/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.contrib import admin #declare admin
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,7 +7,8 @@ urlpatterns = patterns('',
 	url(r'^$', 'photofeeds.views.home' ,name="home"),
 	url(r'^contact/', 'photofeeds.views.contact' ,name="contact"),
 	url(r'^about', 'instagram_clone.views.about' ,name="about"),
-	url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)), #set admin
+	url(r'^accounts/',include('registration.backends.default.urls')),
 #url(r'^rango/', include('rango.urls')),
 #url(r'^accounts/', include('registration.backends.simple.urls')),
 )
